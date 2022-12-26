90fd64bf4e09df6c739231f913ab0ff6437e043d
Bruno Sato <bruno.sato@live.com>
2022-12-26 15:42:49 -0300

    fix

--- pages/_middleware.js
+++
@@ -1,9 +0,0 @@
-/*eslint-disable */
-import { NextResponse } from "next/server";
-
-export function middleware(req) {
-  if (req.nextUrl.pathname === "/delivery") {
-    return NextResponse.redirect("https://www.goomer.app/burton-cervejas-artesanais-1");
-  }
-  return NextResponse.redirect("https://www.instagram.com/burtoncervejaria/");
-}
